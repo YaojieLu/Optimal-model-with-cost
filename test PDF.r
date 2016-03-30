79753ca9bfcd1b7f0b0a3cbf3da1d57ee6535c58
Yaojie Lu <tanisraistlin@gmail.com>
2016-03-31 09:56:46 +1100

    1

---
+++ test PDF.r
@@ -0,0 +1,39 @@
+
+source("test (functions).r")
+
+ca1 <- 400
+k1 <- 0.025
+MAP1 <- 365
+
+w0opt <- w0optf(ca1)
+mu <- muf(ca1, k1, MAP1)
+
+# Functions
+PDFf <- function(ca, k, MAP,
+                 Vcmax=50, cp=30, Km=703, Rd=1, LAI=1,
+                 a=1.6, nZ=0.5, p=43200, l=1.8e-5, h=l*a*LAI/nZ*p, VPD=0.02,
+                 pe=-1.58*10^-3, b=4.38, h2=h/1000, xkmax=5, c=5.71, d=10.05, h3=10,
+                 MDP=MAP/365, gamma=1/(MDP/k/1000)*nZ){
+  
+  optf2 <- Vectorize(function(w)optf1(w, ca, k, MAP))
+  Ev <- function(w){h*VPD*optf2(w)}
+  rEv <- function(w){1/Ev(w)}
+  integralrEv <- Vectorize(function(w){integrate(rEv, w, 1)$value})
+  fnoc <- function(w){1/Ev(w)*exp(-gamma*w-k*integralrEv(w))}
+  integralfnoc <- integrate(fnoc, w0opt, 1)$value
+  c <- 1/integralfnoc
+  message(c)
+  f1 <- function(w){
+    res <- c*fnoc(w)
+    message(w)
+    return(res)}
+  
+  x <- seq(w0opt, 1, by=(1-w0opt)/101)
+  res <- f1(x)
+  return(res)
+}
+y <- PDFf(ca1, k1, MAP1)
+
+# Figure
+#windows(8, 6)
+#par(mgp = c(2.2, 1, 0), xaxs = "i", yaxs = "i", lwd = 2, mar=c(4, 4, 2.5, 4), mfrow=c(1,1))
---
+++ test gs(w).r
@@ -0,0 +1,58 @@
+
+source("test (functions).r")
+
+# w0opt & w0ESS
+ca1 <- 400
+w0opt <- w0optf(ca1)
+w0ESS <- optimize(w0ESSf, c(0.113, 1), ca=ca1, tol=.Machine$double.eps)$minimum
+
+# Figure
+windows(8, 6)
+par(mgp=c(2.2, 1, 0), xaxs="i", yaxs="i", lwd=2, mar=c(4, 4, 2.5, 1), mfrow=c(1,1))
+curve(ESSf2, w0ESS, 1, col="black", type="l",
+      xlab=expression(italic(w)),
+      ylab=expression(italic(g[s])~(mol~m^-2~s^-1)),
+      xlim=c(0, 1), ylim=c(0, 1.3),
+      cex.lab=1.3)
+#abline(v=w0ESS, col="black")
+#abline(v=w0opt, col="red")
+#legend("topright", c("ESS", "Optimal"), lty=c(1, 1), col=c("black", "red"))
+
+# Scenarios
+####################################################################
+k1 <- 0.025
+####################################################################
+MAP1 <- 365
+mu <- muf(ca1, k1, MAP1)
+optf2 <- Vectorize(function(w)optf1(w, ca1, k1, MAP1))
+curve(optf2, w0opt, 1, lty=1, col="red", add=T)
+####################################################################
+MAP1 <- 3650
+mu <- muf(ca1, k1, MAP1)
+optf2 <- Vectorize(function(w)optf1(w, ca1, k1, MAP1))
+curve(optf2, w0opt, 1, lty=2, col="red", add=T)
+####################################################################
+k1 <- 0.05
+####################################################################
+MAP1 <- 365
+mu <- muf(ca1, k1, MAP1)
+optf2 <- Vectorize(function(w)optf1(w, ca1, k1, MAP1))
+curve(optf2, w0opt, 1, lty=1, col="blue", add=T)
+####################################################################
+MAP1 <- 3650
+mu <- muf(ca1, k1, MAP1)
+optf2 <- Vectorize(function(w)optf1(w, ca1, k1, MAP1))
+curve(optf2, w0opt, 1, lty=2, col="blue", add=T)
+####################################################################
+k1 <- 0.1
+####################################################################
+MAP1 <- 365
+mu <- muf(ca1, k1, MAP1)
+optf2 <- Vectorize(function(w)optf1(w, ca1, k1, MAP1))
+curve(optf2, w0opt, 1, lty=1, col="darkgreen", add=T)
+####################################################################
+MAP1 <- 3650
+mu <- muf(ca1, k1, MAP1)
+optf2 <- Vectorize(function(w)optf1(w, ca1, k1, MAP1))
+curve(optf2, w0opt, 1, lty=2, col="darkgreen", add=T)
+####################################################################
